9f95ddcdec2b4ad344fea5983e9db1dd89e191a6
Luna de Oliveira Bezerril Deodato <20241011110004@ifrn.local>
2025-10-30 14:48:43 -0300

    Criação do compontente cards de inscrições abertas

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import Button from './componentes/Button.vue'
+import CardInscricoes from './componentes/CardInscricoes.vue'
+const eventos = [
+  {
+    titulo: 'Evento alusivo ao dia do professor de geografia',
+    local: 'Miniauditório Central',
+    categoria: 'Palestras',
+    datas: [
+      { data: '20/06', hora: '14h', descricao: 'Abertura das inscrições' },
+      { data: '24/06', hora: '23:59', descricao: 'Período final das inscrições' },
+    ],
+  },
+]
 </script>
 
 <template>
@@ -9,6 +21,35 @@ import Button from './componentes/Button.vue'
     documentation
   </p>
   <Button></Button>
+  <CardInscricoes
+    v-for="(evento, index) in eventos"
+    :key="index"
+    :titulo="evento.titulo"
+    :local="evento.local"
+    :categoria="evento.categoria"
+    :datas="evento.datas"
+  >
+  </CardInscricoes>
 </template>
 
-<style scoped></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-size: 1rem;
+  font-family:
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+}
+</style>
---
+++ vue-project/src/componentes/CardInscricoes.vue
@@ -0,0 +1,74 @@
+<script setup>
+defineProps({
+  titulo: String,
+  local: String,
+  categoria: String,
+  datas: Array,
+})
+</script>
+<template>
+  <div class="cardevento">
+    <div class="cardheader">
+      <h3>
+        {{ titulo }}
+      </h3>
+      <p>{{ local }}</p>
+    </div>
+    <div class="cardbody">
+      <ul class="timeline">
+        <li v-for="(item, i) in datas" key="i" :class="{ start: i === 0, end: i === 1 }">
+          <span class="data">{{ item.data }} às {{ item.hora }}</span>
+          <span class="descricao">{{ item.descricao }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped>
+.cardevento {
+  background-image: url(imagens/CardInscricoes_background.png);
+  background-size: cover;
+  background-position: center;
+  max-width: 400px;
+  width: 32%;
+  border-radius: 16px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+.timeline {
+  border-left: 2px solid #222;
+  margin-left: 15px;
+  padding-left: 15px;
+  list-style-type: disc;
+}
+li {
+  padding: 1rem 0;
+  position: relative;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+li.start::before {
+  content: '•';
+  font-size: 2.5rem;
+  position: absolute;
+  left: -23px;
+  top: -3px;
+  color: #009218;
+}
+li.end::before {
+  content: '•';
+  font-size: 2.5rem;
+  position: absolute;
+  left: -23px;
+  top: -3px;
+  color: #c62828;
+}
+.descricao {
+  font-size: 1rem;
+}
+.data {
+  font-size: 0.8rem;
+  font-weight: lighter;
+}
+</style>
