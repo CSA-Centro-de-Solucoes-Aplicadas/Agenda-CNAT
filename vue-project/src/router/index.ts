61e8bbe0d2cb73972e6d2ff79fe8f3b8aa2c6519
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-12-01 14:13:09 -0300

    Limpando route

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,19 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/home.vue'
 import Cadastro from '@/views/cadastro.vue'
-import Modal from '@/componentes/modal.vue'
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/cadastro', component: Cadastro },
-  { path: '/modal', component: Modal },  
-]
-
+const routes = [{ path: '/cadastro', component: Cadastro }]
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
 
 export default router
