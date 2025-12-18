01e7e9c24d112ecb6a93c7100feb56a10f5b48ae
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-12-18 11:00:25 -0300

    revertendo merge do branch

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home.vue'
-import Cadastro from '@/views/cadastro.vue'
-import Modal from '@/componentes/modal.vue'
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/cadastro', component: Cadastro },
-  { path: '/modal', component: Modal },  
+const routes = [{ path: '/home', component: Home }
 ]
-
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
