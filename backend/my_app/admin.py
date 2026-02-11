fd39e003cb696f068779289d3c6affacec319bf2
Erm2k8 <ermesson2k8and@gmail.com>
2026-02-11 18:00:21 -0300

    chore: remove legacy Django backend files

--- backend/my_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Event, Categoria
-
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(Categoria)  
--- backend/my_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import EventViewSet
-
-router = DefaultRouter()
-router.register(r'events', EventViewSet, basename='event')
-# Registra o EventViewSet no router com o prefixo 'events'
-
-urlpatterns = router.urls # Inclui todas as rotas geradas pelo router no prefixo raiz da app
